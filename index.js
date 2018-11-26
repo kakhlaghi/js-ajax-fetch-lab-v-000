3bbac222a8d46ca1be5ab12971a39c5fe4530567
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-11-26 22:51:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ function showResults(json) {
 
 function createIssue() {
   //use this function to create an issue based on the values input in index.html
-  
+  const repo = `${user}/js-ajax-fetch-lab`
 }
 
 function getIssues() {
