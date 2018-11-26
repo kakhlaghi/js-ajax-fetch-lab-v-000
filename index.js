26d41ac5d2b9ad097a3c65f432ba785d1d59c473
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-11-26 22:47:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function forkRepo() {
 
 function showResults(json) {
   //use this function to display the results from forking via the API
-  document.getElementById('results')
+  document.getElementById('results').innerHTML
 }
 
 function createIssue() {
